c609561e005f60c9ec7dd3e982e6b379f6cbb5e6
Angus Stanton <angusstanton225@gmail.com>
2019-11-14 18:56:39 +0000

    Update demo_gpio_counter so it correctly counts pulses

--- src/demo_gpio_counter.cpp
+++ src/demo_gpio_counter.cpp
@@ -8,22 +8,33 @@
 using hyped::utils::io::GPIO;
 using hyped::utils::Logger;
 
-constexpr int kMaxPulse = 1000;
+constexpr int kMaxPulse = 8000;
 
 int main(int argc, char* argv[]) {
   hyped::utils::System::parseArgs(argc, argv);
   Logger log(true, 0);
 
   GPIO gpio(67, hyped::utils::io::gpio::kIn);
-
+  int pin = 0;
   int count = 0;
   int prev = count;
-
+  log.INFO("GPIOTEST", "Test started, initial count");
   while (count < kMaxPulse) {
-    count += gpio.read();       // read() faster than wait()
-    if (count > prev) {
-      // pulse read
-    }  
+    while(pin == 1){
+      //log.INFO("GPIOTEST", "pin is 1");
+      pin = gpio.read();
+
+    }
+
+    while(pin == 0){
+      //log.INFO("GPIOTEST", "pin is 0");
+
+      pin = gpio.read();
+
+    }
+    //log.INFO("GPIOTEST", "Counter %d", count);
+    count++;       
+
   }
   log.INFO("GPIOTEST", "Final pulse count %d", count);
   
