2c9f652e0a425b4be542514063d0dd44be4d87dc
Calum McM <calummcmeekin@me.com>
2020-01-16 17:48:58 +0000

    Added Excercise 2

---
+++ src/criticalsystems/abobay.hpp
@@ -0,0 +1,38 @@
+/*
+ * Authors: Calum McMeekin
+ * Organisation: HYPED
+ * Date:
+ * Description:
+ * HypedMachine wraps around State objects, i.e. functions as a State manager.
+ * HypedMachine reacts to events through handleEvent() function. Fucntion transition()
+ * finalises changes of current state and facilitate updates to the shared data structure.
+ *
+ *    Copyright 2019 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef CRITICALSYSTEMS_ABOBAY_HPP_
+#define CRITICALSYSTEMS_ABOBAY_HPP_
+
+#include "rectangle.hpp"
+
+class Abobay
+{
+    // Add your global variables here
+
+  public:
+    // Add your functions here
+};
+
+
+#endif  // CRITICALSYSTEMS_ABOBAY_HPP_
