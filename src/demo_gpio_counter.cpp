59bbadf406340d82efa9b76e7ad294234c3681b6
EneaZer0 <miguezsergio22@gmail.com>
2020-02-10 19:46:51 +0000

    Update counter

--- src/demo_gpio_counter.cpp
+++ src/demo_gpio_counter.cpp
@@ -1,4 +1,3 @@
-
 #include "utils/concurrent/thread.hpp"
 #include "utils/io/gpio.hpp"
 #include "utils/logger.hpp"
@@ -7,35 +6,26 @@
 
 using hyped::utils::io::GPIO;
 using hyped::utils::Logger;
-
+using hyped::utils::System;
 constexpr int kMaxPulse = 8000;
 
 int main(int argc, char* argv[]) {
   hyped::utils::System::parseArgs(argc, argv);
-  Logger log(true, 0);
-
-  GPIO gpio(67, hyped::utils::io::gpio::kIn);
+  Logger log = System::getLogger(); 
+  GPIO gpio(67, hyped::utils::io::gpio::kIn);     // pin 67 as read
   int pin = 0;
   int count = 0;
-  int prev = count;
-  log.INFO("GPIOTEST", "Test started, initial count");
-  while (count < kMaxPulse) {
-    while(pin == 1){
-      //log.INFO("GPIOTEST", "pin is 1");
-      pin = gpio.read();
-
-    }
-
-    while(pin == 0){
-      //log.INFO("GPIOTEST", "pin is 0");
-
-      pin = gpio.read();
-
-    }
-    //log.INFO("GPIOTEST", "Counter %d", count);
-    count++;       
-
-  }
-  log.INFO("GPIOTEST", "Final pulse count %d", count);
+  log.INFO("Gpio_counter_test", "Test started, initial count");
   
+  while (count < kMaxPulse) {
+    while (!pin) {
+      gpio.read();
+    }
+     while (pin == 1) {
+      pin = gpio.read();
+    }
+    count++;      // this should only increase after reading high and low pulse consecutively
+    log.INFO("Gpio_counter_test", "count: %d", count);
+  }
+  log.INFO("Gpio_counter_test", "Final pulse count %d", count);
 } // end of main
