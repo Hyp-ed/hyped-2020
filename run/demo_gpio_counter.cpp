1e9d5904a8bcc35e8f99f3ed7ad5dfc2774326be
Branislav Pilnan <B.Pilnan@gmail.com>
2020-02-13 15:41:09 +0000

    Add busy thread and timed termination

--- run/demo_gpio_counter.cpp
+++ run/demo_gpio_counter.cpp
@@ -2,30 +2,46 @@
 #include "utils/io/gpio.hpp"
 #include "utils/logger.hpp"
 #include "utils/system.hpp"
+#include "utils/timer.hpp"
 #include "data/data.hpp"
 
+using hyped::utils::concurrent::BusyThread;
+using hyped::utils::concurrent::Thread;
 using hyped::utils::io::GPIO;
 using hyped::utils::Logger;
 using hyped::utils::System;
-constexpr int kMaxPulse = 8000;
+using hyped::utils::Timer;
+
+constexpr int kMaxTime = 4;
 
 int main(int argc, char* argv[]) {
   hyped::utils::System::parseArgs(argc, argv);
-  Logger log = System::getLogger(); 
+  Logger log = System::getLogger();
+
+  BusyThread* worker = new BusyThread();
+  worker->start();
+
   GPIO gpio(67, hyped::utils::io::gpio::kIn);     // pin 67 as read
   int pin = 0;
   int count = 0;
   log.INFO("Gpio_counter_test", "Test started, initial count");
-  
-  while (count < kMaxPulse) {
+
+  uint64_t t0 = Timer::getTimeMicros();
+  while (Timer::getTimeMicros() - t0 < kMaxTime*1000000) {
     while (!pin) {
       gpio.read();
     }
      while (pin == 1) {
       pin = gpio.read();
-    }
-    count++;      // this should only increase after reading high and low pulse consecutively
-    log.INFO("Gpio_counter_test", "count: %d", count);
+    }if (count%10000 == 0)
+      log.INFO("Gpio_counter_test", "count: %5dk, time: %fs", count/1000,
+                                                              (Timer::getTimeMicros() - t0)/1e6);
+    count++;  // this should only increase after reading high and low pulse consecutively
   }
+
+  log.INFO("Gpio_counter_test", "Stopping the worker");
+  // TODO(Brano): Stop BusyThread via System::running_
+  delete worker;
+
   log.INFO("Gpio_counter_test", "Final pulse count %d", count);
 } // end of main
