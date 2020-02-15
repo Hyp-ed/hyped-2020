5c6193e25259aa389e226a53d8bda683b757da83
Branislav Pilnan <B.Pilnan@gmail.com>
2020-02-15 15:16:09 +0000

    Move counting to a separate thread

--- run/demo_gpio_counter.cpp
+++ run/demo_gpio_counter.cpp
@@ -1,3 +1,6 @@
+#include <atomic>
+#include <cstdio>
+
 #include "utils/concurrent/thread.hpp"
 #include "utils/io/gpio.hpp"
 #include "utils/logger.hpp"
@@ -14,35 +17,73 @@ using hyped::utils::Timer;
 
 constexpr int kMaxTime = 32;
 
+class SimpleCounter : public Thread {
+ public:
+  SimpleCounter();
+
+  void run() override;
+  int get_count() { return count_.load(); }
+  void reset_count() { count_ = 0; }
+
+ private:
+  System&         sys_;
+  std::atomic_int count_;
+};
+
+SimpleCounter::SimpleCounter() : sys_(System::getSystem()), count_(0) { /* EMPTY */ }
+
+void SimpleCounter::run()
+{
+  GPIO gpio(67, hyped::utils::io::gpio::kIn);  // pin 67 as read
+  int pin = 0;
+  while (sys_.running_) {
+    while (!pin && sys_.running_) {
+      pin = gpio.read();
+    }
+    while (pin == 1 && sys_.running_) {
+      pin = gpio.read();
+    }
+    ++count_;
+  }
+}
+
 int main(int argc, char* argv[]) {
   hyped::utils::System::parseArgs(argc, argv);
-  Logger log = System::getLogger();
+  Logger  log = System::getLogger();
+  System& sys = System::getSystem();
 
   BusyThread* worker = new BusyThread();
   worker->start();
 
-  GPIO gpio(67, hyped::utils::io::gpio::kIn);     // pin 67 as read
-  int pin = 0;
-  int count = 0;
-  log.INFO("Gpio_counter_test", "Test started, initial count");
+  SimpleCounter counter;
+  counter.start();
 
-  uint64_t t0 = Timer::getTimeMicros();
-  while (Timer::getTimeMicros() - t0 < kMaxTime*1000000) {
-    while (!pin) {
-      pin = gpio.read();
-    }
-     while (pin == 1) {
-      pin = gpio.read();
-    }
-    if (count%1000 == 0)
-      log.INFO("Gpio_counter_test", "count: %5dk, time: %fs", count/1000,
-                                                              (Timer::getTimeMicros() - t0)/1e6);
-    count++;  // this should only increase after reading high and low pulse consecutively
-  }
+  Thread::sleep(500);
+  char input;
+  do {
+    std::printf("Enter c to display pulse count, r to reset the count to 0, or q to quit: ");
+    // Read response, ignoring newlines
+     do {
+      int num_read = std::scanf("%c", &input);  // Read user input
+      if (num_read != 1) {
+        log.ERR("Gpio_counter_test", "scanf filled %d arguments instead of 1", num_read);
+        break;
+      }
+    } while (input == '\n' || input == '\r');
+
+    if (input == 'c')
+      std::printf("Pulse count: %d\n\n", counter.get_count());
+    if (input == 'r')
+      counter.reset_count();
+  } while (input != 'q');
+
+  log.INFO("Gpio_counter_test", "Stopping the counter");
+  sys.running_ = false;
+  counter.join();
 
   log.INFO("Gpio_counter_test", "Stopping the worker");
   // TODO(Brano): Stop BusyThread via System::running_
   delete worker;
 
-  log.INFO("Gpio_counter_test", "Final pulse count %d", count);
+  log.INFO("Gpio_counter_test", "Final pulse count %d", counter.get_count());
 } // end of main
